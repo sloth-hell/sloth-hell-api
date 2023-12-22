ef78e3940412969948e8fb6a0b6b1958aa65d193
lemphis <lemphis@gmail.com>
2023-12-22 17:48:35 +0900

    Add JwtProperties
    * JWT 관련 properties 매핑 클래스 추가

---
+++ src/main/kotlin/me/lemphis/slothhell/global/security/JwtProperties.kt
@@ -0,0 +1,14 @@
+package me.lemphis.slothhell.global.security
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("application.security.jwt")
+data class JwtProperties(
+	val secretKey: String,
+	val expiration: Expiration,
+)
+
+data class Expiration(
+	val accessToken: Long,
+	val refreshToken: Long,
+)
