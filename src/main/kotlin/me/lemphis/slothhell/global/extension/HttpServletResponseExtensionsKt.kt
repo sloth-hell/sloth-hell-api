19ca95355fab3923bad44bc297ff825633a09c1e
lemphis <lemphis@gmail.com>
2024-01-01 17:20:36 +0900

    Add HttpServletResponseExtensionsKt
    * HttpServletResponse의 확장 함수들 추가

---
+++ src/main/kotlin/me/lemphis/slothhell/global/extension/HttpServletResponseExtensionsKt.kt
@@ -0,0 +1,23 @@
+package me.lemphis.slothhell.global.extension
+
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import java.nio.charset.StandardCharsets
+
+fun HttpServletResponse.createUnauthorizedResponse(responseBody: String) {
+	status = HttpServletResponse.SC_UNAUTHORIZED
+	setResponseBody(responseBody)
+}
+
+fun HttpServletResponse.createForbiddenResponse(responseBody: String) {
+	status = HttpServletResponse.SC_FORBIDDEN
+	setResponseBody(responseBody)
+}
+
+private fun HttpServletResponse.setResponseBody(responseBody: String) {
+	contentType = MediaType.APPLICATION_JSON_VALUE
+	characterEncoding = StandardCharsets.UTF_8.name()
+	writer.write(responseBody)
+	writer.flush()
+	writer.close()
+}
