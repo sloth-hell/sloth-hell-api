80918fa5ff6ad6241f13760339f1a5118fbb7d68
lemphis <lemphis@gmail.com>
2024-01-02 22:34:38 +0900

    Add ObjectMapper bean config
    * ObjectMapper 빈 설정 추가

---
+++ src/main/kotlin/me/lemphis/slothhell/global/config/ObjectMapperConfig.kt
@@ -0,0 +1,19 @@
+package me.lemphis.slothhell.global.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ObjectMapperConfig {
+
+	@Bean
+	fun objectMapper() = ObjectMapper()
+		.registerModule(JavaTimeModule())
+		.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+		.registerKotlinModule()
+
+}
