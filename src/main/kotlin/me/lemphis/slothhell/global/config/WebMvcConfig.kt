1608506b44dbde4aea5f4c7ed9280c0e90d2ea8e
lemphis <lemphis@gmail.com>
2024-01-02 21:56:21 +0900

    Remove @EnableWebMvc from WebConfig
    * @EnableWebMvc 제거(web.resources.add-mappings 설정으로 대체)

--- src/main/kotlin/me/lemphis/slothhell/global/config/WebMvcConfig.kt
+++ src/main/kotlin/me/lemphis/slothhell/global/config/WebMvcConfig.kt
@@ -1,14 +1,12 @@
-package me.lemphis.slothhell.global.web
+package me.lemphis.slothhell.global.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-@EnableWebMvc
 @Profile("!prod")
 class WebMvcConfig : WebMvcConfigurer {
 
