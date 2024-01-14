e9caa9c04c51f4b415afda939a3420580054853d
lemphis <lemphis@gmail.com>
2024-01-14 23:15:44 +0900

    Add createRedirectResponse extension function to HttpServletResponseExtensionsKt
    * HttpServletResponseExtensionsKt 파일에 createRedirectResponse 확장 함수 추가

--- src/main/kotlin/me/lemphis/slothhell/config/extension/HttpServletResponseExtensionsKt.kt
+++ src/main/kotlin/me/lemphis/slothhell/config/extension/HttpServletResponseExtensionsKt.kt
@@ -1,6 +1,7 @@
 package me.lemphis.slothhell.config.extension
 
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import java.nio.charset.StandardCharsets
 
@@ -11,6 +12,12 @@ fun HttpServletResponse.createUnauthorizedResponse(responseBody: String) {
 
 fun HttpServletResponse.createForbiddenResponse(responseBody: String) {
 	status = HttpServletResponse.SC_FORBIDDEN
+	setResponseBody(responseBody)
+}
+
+fun HttpServletResponse.createRedirectResponse(responseBody: String, redirectUrl: String) {
+	status = HttpServletResponse.SC_MOVED_TEMPORARILY
+	setHeader(HttpHeaders.LOCATION, redirectUrl)
 	setResponseBody(responseBody)
 }
 
