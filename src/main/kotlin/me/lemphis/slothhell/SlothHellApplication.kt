b7f02288af143f4568cd8055eb67f2a9e17134e4
lemphis <lemphis@gmail.com>
2024-01-31 12:59:03 +0900

    Add logger
    * log 생성 메서드 추가

--- src/main/kotlin/me/lemphis/slothhell/SlothHellApplication.kt
+++ src/main/kotlin/me/lemphis/slothhell/SlothHellApplication.kt
@@ -1,10 +1,14 @@
 package me.lemphis.slothhell
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class SlothHellApplication
+
+inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
 
 fun main(args: Array<String>) {
 	runApplication<SlothHellApplication>(*args)
