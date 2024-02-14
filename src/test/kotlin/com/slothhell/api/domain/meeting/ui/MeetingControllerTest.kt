c0bfe9b49033364e2d6e440a24cd4c08ddbe2612
lemphis <lemphis@gmail.com>
2024-02-14 22:52:49 +0900

    Update test code
    * CreateMeetingRequest parameter 추가로 인한 테스트 코드 수정 및 응답 필드 추가
    * @AuthenticationPrincipal에 대한 추가 테스트 로직 필요

--- src/test/kotlin/com/slothhell/api/domain/meeting/ui/MeetingControllerTest.kt
+++ src/test/kotlin/com/slothhell/api/domain/meeting/ui/MeetingControllerTest.kt
@@ -1,8 +1,9 @@
-package com.slothhell.api.domain.meeting.web.web
+package com.slothhell.api.domain.meeting.ui
 
 import com.slothhell.api.BaseControllerTest
 import com.slothhell.api.meeting.application.CreateMeetingRequest
 import com.slothhell.api.meeting.application.MeetingService
+import com.slothhell.api.meeting.domain.ConversationType
 import com.slothhell.api.meeting.ui.MeetingController
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -19,7 +20,6 @@ import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
-import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 import java.time.LocalDateTime
 
@@ -30,121 +30,23 @@ class MeetingControllerTest : BaseControllerTest() {
 	lateinit var meetingService: MeetingService
 
 	@Test
-	@DisplayName("[GET /meeting/{meetingId}] 존재하는 meetingId로 요청 시 200 응답")
-	fun givenExistingMeetingId_whenGetMeeting_thenRetrieveMeetingInfo() {
-		val invalidKakaoChatUrl = "https://open.kakao.com/o/1234567"
-		val createMeetingRequest = CreateMeetingRequest(
-			title = "모각코 4인팟 모집",
-			location = "스타벅스 과천DT점",
-			startedAt = LocalDateTime.now().plusDays(1),
-			kakaoChatUrl = invalidKakaoChatUrl,
-		)
-		val accessToken = jwtAuthenticationProvider.generateAccessToken("test")
-
-		given(meetingService.createMeeting(createMeetingRequest)).willReturn(1L)
-
-		mockMvc.get("/meetings") {
-			contentType = MediaType.APPLICATION_JSON
-			content = objectMapper.writeValueAsString(createMeetingRequest)
-			header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
-		}.andExpect {
-			status { isBadRequest() }
-			content {
-				jsonPath("$.errorField") { value("kakaoChatUrl") }
-				jsonPath("$.receivedValue") { value(invalidKakaoChatUrl) }
-				jsonPath("$.message") { value("올바른 카카오톡 오픈채팅 URL 형식이 아닙니다.") }
-			}
-		}.andDo {
-			handle(
-				document(
-					"meeting/create-error",
-					requestHeaders(
-						headerWithName(HttpHeaders.AUTHORIZATION).description("access token"),
-						headerWithName(HttpHeaders.CONTENT_TYPE).description("${MediaType.APPLICATION_JSON} 고정"),
-					),
-					requestFields(
-						fieldWithPath("title").description("모임 제목"),
-						fieldWithPath("location").description("모임 장소"),
-						fieldWithPath("startedAt").description("모임 시각"),
-						fieldWithPath("kakaoChatUrl").description("카카오톡 오픈채팅 URL"),
-					),
-					responseHeaders(
-						headerWithName(HttpHeaders.CONTENT_TYPE).description("${MediaType.APPLICATION_JSON} 고정"),
-					),
-					responseFields(
-						fieldWithPath("errorField").type(JsonFieldType.STRING).description("이슈가 발생한 필드"),
-						fieldWithPath("receivedValue").type(JsonFieldType.STRING).description("서버가 받은 이슈가 발생한 필드 값"),
-						fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
-					),
-				),
-			)
-		}
-	}
-
-	@Test
-	@DisplayName("[GET /meeting] meeting 목록 요청 시 200 응답")
-	fun givenMeetingsExist_whenGetMeetings_thenRetrieveMeetings() {
-		val invalidKakaoChatUrl = "https://open.kakao.com/o/1234567"
-		val createMeetingRequest = CreateMeetingRequest(
-			title = "모각코 4인팟 모집",
-			location = "스타벅스 과천DT점",
-			startedAt = LocalDateTime.now().plusDays(1),
-			kakaoChatUrl = invalidKakaoChatUrl,
-		)
-		val accessToken = jwtAuthenticationProvider.generateAccessToken("test")
-
-		given(meetingService.createMeeting(createMeetingRequest)).willReturn(1L)
-
-		mockMvc.post("/meetings") {
-			contentType = MediaType.APPLICATION_JSON
-			content = objectMapper.writeValueAsString(createMeetingRequest)
-			header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
-		}.andExpect {
-			status { isBadRequest() }
-			content {
-				jsonPath("$.errorField") { value("kakaoChatUrl") }
-				jsonPath("$.receivedValue") { value(invalidKakaoChatUrl) }
-				jsonPath("$.message") { value("올바른 카카오톡 오픈채팅 URL 형식이 아닙니다.") }
-			}
-		}.andDo {
-			handle(
-				document(
-					"meeting/create-error",
-					requestHeaders(
-						headerWithName(HttpHeaders.AUTHORIZATION).description("access token"),
-						headerWithName(HttpHeaders.CONTENT_TYPE).description("${MediaType.APPLICATION_JSON} 고정"),
-					),
-					requestFields(
-						fieldWithPath("title").description("모임 제목"),
-						fieldWithPath("location").description("모임 장소"),
-						fieldWithPath("startedAt").description("모임 시각"),
-						fieldWithPath("kakaoChatUrl").description("카카오톡 오픈채팅 URL"),
-					),
-					responseHeaders(
-						headerWithName(HttpHeaders.CONTENT_TYPE).description("${MediaType.APPLICATION_JSON} 고정"),
-					),
-					responseFields(
-						fieldWithPath("errorField").type(JsonFieldType.STRING).description("이슈가 발생한 필드"),
-						fieldWithPath("receivedValue").type(JsonFieldType.STRING).description("서버가 받은 이슈가 발생한 필드 값"),
-						fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
-					),
-				),
-			)
-		}
-	}
-
-	@Test
 	@DisplayName("[POST /meeting/{meetingId}] 정상 요청 시 201 응답")
 	fun givenValidCreateMeetingRequest_whenCreateMeeting_thenReturnCreatedStatusAndLocationHeader() {
+		val userId = 1L
 		val createMeetingRequest = CreateMeetingRequest(
 			title = "모각코 4인팟 모집",
 			location = "스타벅스 과천DT점",
 			startedAt = LocalDateTime.now().plusDays(5),
 			kakaoChatUrl = "https://open.kakao.com/o/12345678",
+			allowedGender = null,
+			minAge = 20,
+			maxAge = null,
+			description = "모여서 각자 코딩하실 3분을 더 모집합니다!",
+			conversationType = ConversationType.LIGHT_CONVERSATION,
 		)
-		val accessToken = jwtAuthenticationProvider.generateAccessToken("test")
+		val accessToken = jwtAuthenticationProvider.generateAccessToken(userId)
 
-		given(meetingService.createMeeting(createMeetingRequest)).willReturn(1L)
+		given(meetingService.createMeeting(createMeetingRequest, userId)).willReturn(1L)
 
 		mockMvc.post("/meetings") {
 			contentType = MediaType.APPLICATION_JSON
@@ -167,6 +69,11 @@ class MeetingControllerTest : BaseControllerTest() {
 						fieldWithPath("location").description("모임 장소"),
 						fieldWithPath("startedAt").description("모임 시각"),
 						fieldWithPath("kakaoChatUrl").description("카카오톡 오픈채팅 URL"),
+						fieldWithPath("allowedGender").optional().description("모임에 참여 가능한 성별"),
+						fieldWithPath("minAge").optional().description("모임에 참여 가능한 최소 연령"),
+						fieldWithPath("maxAge").optional().description("모임에 참여 가능한 최대 연령"),
+						fieldWithPath("description").optional().description("모임에 참여 가능한 최대 연령"),
+						fieldWithPath("conversationType").description("대화할 수 있는 정도"),
 					),
 					responseHeaders(
 						headerWithName(HttpHeaders.CONTENT_TYPE).description("${MediaType.APPLICATION_JSON} 고정"),
@@ -183,16 +90,22 @@ class MeetingControllerTest : BaseControllerTest() {
 	@Test
 	@DisplayName("[POST /meeting/{meetingId}] validation 통과하지 못하는 요청을 받은 경우 400 응답")
 	fun givenInvalidCreateMeetingRequest_whenCreateMeeting_thenReturnBadRequestStatus() {
+		val userId = 1L
 		val invalidKakaoChatUrl = "https://open.kakao.com/o/1234567"
 		val createMeetingRequest = CreateMeetingRequest(
 			title = "모각코 4인팟 모집",
 			location = "스타벅스 과천DT점",
-			startedAt = LocalDateTime.now().plusDays(1),
+			startedAt = LocalDateTime.now().plusDays(5),
 			kakaoChatUrl = invalidKakaoChatUrl,
+			allowedGender = null,
+			minAge = 20,
+			maxAge = null,
+			description = null,
+			conversationType = ConversationType.LIGHT_CONVERSATION,
 		)
-		val accessToken = jwtAuthenticationProvider.generateAccessToken("test")
+		val accessToken = jwtAuthenticationProvider.generateAccessToken(userId)
 
-		given(meetingService.createMeeting(createMeetingRequest)).willReturn(1L)
+		given(meetingService.createMeeting(createMeetingRequest, userId)).willReturn(1L)
 
 		mockMvc.post("/meetings") {
 			contentType = MediaType.APPLICATION_JSON
@@ -218,6 +131,11 @@ class MeetingControllerTest : BaseControllerTest() {
 						fieldWithPath("location").description("모임 장소"),
 						fieldWithPath("startedAt").description("모임 시각"),
 						fieldWithPath("kakaoChatUrl").description("카카오톡 오픈채팅 URL"),
+						fieldWithPath("allowedGender").optional().description("모임에 참여 가능한 성별"),
+						fieldWithPath("minAge").optional().description("모임에 참여 가능한 최소 연령"),
+						fieldWithPath("maxAge").optional().description("모임에 참여 가능한 최대 연령"),
+						fieldWithPath("description").optional().description("모임에 참여 가능한 최대 연령"),
+						fieldWithPath("conversationType").description("대화할 수 있는 정도"),
 					),
 					responseHeaders(
 						headerWithName(HttpHeaders.CONTENT_TYPE).description("${MediaType.APPLICATION_JSON} 고정"),
