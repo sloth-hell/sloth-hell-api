9cecdf2c4a516bcd1214f60e07e506d6fee9d4fa
lemphis <lemphis@gmail.com>
2024-04-05 13:00:47 +0900

    Add CORS mapping
    * CORS 관련 설정 추가

--- src/main/kotlin/com/slothhell/api/config/web/WebMvcConfig.kt
+++ src/main/kotlin/com/slothhell/api/config/web/WebMvcConfig.kt
@@ -2,6 +2,7 @@ package com.slothhell.api.config.web
 
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -14,6 +15,12 @@ class WebMvcConfig : WebMvcConfigurer {
 		registry
 			.addResourceHandler("/swagger-ui/**")
 			.addResourceLocations("classpath:/static/swagger-ui/")
+	}
+
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://sloth-hell.com")
+			.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
 	}
 
 }
